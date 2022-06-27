8905d1926a4d0a1ebcf190acfc42af5d1388651e
Max Miller <67687590+rlmtm@users.noreply.github.com>
2022-06-27 03:08:00 +0200

    Add files via upload

---
+++ Tab Sections/Home View/Content Subviews/Courses/Medicine/Medicine Subviews/Medical Course Requirements/MedCR2.swift
@@ -0,0 +1,38 @@
+//
+//  MedCR2.swift
+//  Uniply
+//
+//  Created by MILLER, Maximilian on 08/01/2022.
+//  Copyright © 2022 Balaji. All rights reserved.
+//
+
+import SwiftUI
+
+struct MedCR2: View {
+    var body: some View {
+        Text("Academic Requirements - US:")
+            .font(.title3.weight(.bold))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 20)
+        
+        ZStack {
+            Rectangle()
+                .frame(width: 370, height: 30, alignment: .center)
+                .cornerRadius(10)
+                .foregroundColor(.white)
+            Text("""
+                To be added later...
+                """)
+                .multilineTextAlignment(.leading)
+                .padding(.horizontal, 29)
+                .frame(width: 380, height: 30, alignment: .leading)
+                .font(.system(size: 16))
+        }
+    }
+}
+
+struct MedCR2_Previews: PreviewProvider {
+    static var previews: some View {
+        MedCR2()
+    }
+}
