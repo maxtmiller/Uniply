e620058d9f9edda184fd46d6937c4af4fb2aa96c
Max Miller <67687590+rlmtm@users.noreply.github.com>
2022-06-27 03:15:42 +0200

    Update ComputerScienceDetailView.swift

--- Tab Sections/Home View/Content Subviews/Courses/Computer Science/ComputerScienceDetailView.swift
+++ Tab Sections/Home View/Content Subviews/Courses/Computer Science/ComputerScienceDetailView.swift
@@ -48,7 +48,7 @@ struct ComputerScienceDetailView: View {
                           .padding(8)
                           .background(.ultraThinMaterial, in: Circle())
                     }
-                    .padding(.top, -50)
+                    //.padding(.top, -50)
                     .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
                     .padding(20)
                     .ignoresSafeArea()
@@ -56,7 +56,7 @@ struct ComputerScienceDetailView: View {
                 overlay
                     .padding(.bottom, -180)
                 Rectangle()
-                    .frame(width: 380, height: 100, alignment: .center)
+                    .frame(width: 380, height: 150, alignment: .center)
                     .opacity(0)
             }
             /*.overlay(
@@ -79,6 +79,7 @@ struct ComputerScienceDetailView: View {
             .navigationBarHidden(true)
             .statusBar(hidden: true)
         }
+        .frame(width: 400, height: 860)
     }
     
     var cover: some View {
