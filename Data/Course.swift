d4b395764887d00022f6adabaf65238410afde70
Max Miller <67687590+rlmtm@users.noreply.github.com>
2022-06-27 02:32:12 +0200

    Update Course.swift

--- Data/Course.swift
+++ Data/Course.swift
@@ -22,5 +22,5 @@ var courses = [
     Course(title: "Computer Science", subtitle: "20 sections - 3 hours", text: "Learn the process of getting a major in this field and more ...", image: "1", color: .purple),
     Course(title: "Business", subtitle: "20 sections - 3 hours", text: "Learn the process of getting a major in this field and more ...", image: "1", color: .green),
     Course(title: "Law Studies", subtitle: "20 sections - 3 hours", text: "Learn the process of getting a major in this field and more ...", image: "1", color: .blue),
-    Course(title: "University Application", subtitle: "1 sections - 5 minutes", text: "This section will explain about the information needed to apply to universities...", image: "Application", color: .blue),
+    Course(title: "University Application Form", subtitle: "1 sections - 5 minutes", text: "This section will provide an application form template which can be used for many universities...", image: "Application", color: .blue),
 ]
